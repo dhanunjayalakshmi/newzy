763fa29781e91ec1d17ce82565f6019cf7477347
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2024-06-12 23:34:49 +0530

    Static Header section is completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Body from "./components/Body";
+
 function App() {
-  return <div className="App">Welcome to NEWZY</div>;
+  return (
+    <div className="App w-full h-screen flex flex-col font-sans overflow-hidden">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+  return (
+    <div className="w-full flex items-center bg-[#2b2d42]">
+      <div className="w-full flex justify-center items-center text-white py-8 mx-auto">
+        <h1 className="font-medium text-4xl">Newzy</h1>
+        <ul className="flex gap-6 text-lg font-semibold ml-40">
+          <li>Sport</li>
+          <li>Health</li>
+          <li>Political</li>
+          <li>Business</li>
+          <li>Finance</li>
+          <li>Life</li>
+          <li>Entertainment</li>
+        </ul>
+        <div className="flex justify-between ml-40">
+          <p>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="text-xl" />
+          </p>
+
+          <div className="flex justify-between gap-6 text-md font-normal ml-16">
+            <p>Login</p>
+            <p>Register</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
