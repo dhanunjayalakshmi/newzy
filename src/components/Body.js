a9c90c4beba14166d514e140e596184067b208e5
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2024-06-28 20:28:04 +0530

    Created a page for latest releases

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HotTopic from "./HotTopic";
+import LatestReleases from "./LatestReleases";
 
 const Body = () => {
   return (
     <div className="w-full flex-1 flex-col items-center mt-4">
       <HotTopic />
+      <LatestReleases />
     </div>
   );
 };
---
+++ src/components/LatestReleases.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LatestReleases = () => {
+  return (
+    <div className="w-full flex justify-center mt-4">
+      <div className="w-full max-w-screen-xl flex flex-col justify-center py-4 mx-auto">
+        <h1 className="font-medium text-4xl">Latest Releases</h1>
+      </div>
+    </div>
+  );
+};
+
+export default LatestReleases;
