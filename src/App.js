ad8e5aa432b54e6435c7b26e442c0a20f1202f60
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2024-07-10 16:42:56 +0530

    Installed React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Body from "./components/Body";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+const RoutesConfig = () => {
+  return useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/login", element: <Login /> },
+    { path: "/register", element: <Register /> },
+  ]);
+};
 
 function App() {
   return (
-    <div className="App w-full h-screen flex flex-col bg-white">
-      <Header />
-      <Body />
-      <Footer />
+    <div>
+      <Router>
+        <div className="App w-full h-screen flex flex-col bg-white">
+          <Header />
+          <RoutesConfig />
+          <Footer />
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="w-full flex items-center bg-[#2b2d42]">
       <div className="w-full max-w-screen-xl flex justify-center items-center text-white py-8 mx-auto">
-        <h1 className="font-bold text-4xl">Newzy</h1>
+        <Link to="/">
+          <h1 className="font-bold text-4xl cursor-pointer">Newzy</h1>
+        </Link>
+
         <ul className="flex gap-6 text-lg font-semibold ml-40">
           <li>Sport</li>
           <li>Health</li>
@@ -22,8 +26,12 @@ const Header = () => {
           </p>
 
           <div className="flex justify-between gap-6 text-sm font-normal ml-16">
-            <p>Login</p>
-            <p>Register</p>
+            <Link to="/login">
+              <p>Login</p>
+            </Link>
+            <Link to="/register">
+              <p>Register</p>
+            </Link>
           </div>
         </div>
       </div>
