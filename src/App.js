99fc23e2afd1bc55b1b47f8e955e01aef1153226
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2024-07-11 21:17:37 +0530

    Ui implementation is completed for the forgot password page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import ForgotPassword from "./components/ForgotPassword";
 
 const RoutesConfig = () => {
   return useRoutes([
     { path: "/", element: <Home /> },
     { path: "/login", element: <Login /> },
     { path: "/register", element: <Register /> },
+    { path: "/forgotPassword", element: <ForgotPassword /> },
   ]);
 };
 
