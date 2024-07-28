14a2066fb030add45b8d30e9d69932e66f87ddc4
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2024-07-29 04:17:06 +0530

    Working on forgot password

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../utils/firebase";
+import { sendPasswordResetEmail } from "firebase/auth";
 
 const ForgotPassword = () => {
+  const [errorMesg, setErrorMesg] = useState(null);
+  const [successMesg, setSuccessMesg] = useState(null);
+  const email = useRef(null);
+
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+
+    try {
+      await sendPasswordResetEmail(auth, email);
+      setSuccessMesg("Password reset email sent successfully!");
+      console.log("Password reset email sent successfully!");
+    } catch (error) {
+      console.error("Error sending password reset email:", error?.message);
+      setErrorMesg("Error sending password reset email"); // Let the caller handle the error, possibly to show UI feedback
+    }
+  };
   return (
     <div className="w-full max-w-screen-xl flex justify-center my-20 py-4 mx-auto ">
       <div className="flex">
@@ -16,12 +34,24 @@ const ForgotPassword = () => {
           </p>
         </div>
         <div className="w-[45%] flex flex-col items-end gap-8">
-          <div className="w-[90%] flex flex-col items-center p-8 border border-black gap-10">
+          <form
+            onSubmit={handleResetPassword}
+            className="w-[90%] flex flex-col items-center p-8 border border-black gap-10"
+          >
+            {errorMesg && (
+              <p className="text-xl font-semibold text-red-500">{errorMesg}</p>
+            )}
+            {successMesg && (
+              <p className="text-xl font-semibold text-green-500">
+                {successMesg}
+              </p>
+            )}
             <h2 className="text-4xl font-bold">Newzy</h2>
             <div className="w-[90%] flex flex-col gap-6">
               <label htmlFor="email" className="flex flex-col gap-2">
                 Email
                 <input
+                  ref={email}
                   type="email"
                   name="email"
                   id="email"
@@ -39,7 +69,7 @@ const ForgotPassword = () => {
                 Here
               </Link>{" "}
             </p>
-          </div>
+          </form>
         </div>
       </div>
     </div>
